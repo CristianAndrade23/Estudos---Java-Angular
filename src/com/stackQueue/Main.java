f6153d6767f56be64746e9e3646dec7984c68915
cristian <cristian.a.s.andrade@hotmail.com>
2019-06-05 13:11:11 -0300

    dia 05/06

---
+++ src/com/stackQueue/Main.java
@@ -0,0 +1,37 @@
+package com.stackQueue;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        ArrayList<Number> arrayNumbers = new ArrayList<Number>();
+
+
+        arrayList.add("Cristian");
+        arrayList.add("Rafaela");
+
+        arrayNumbers.add(3);
+        arrayNumbers.add(4);
+        arrayNumbers.add(5);
+
+        System.out.println(arrayList);
+        System.out.println(arrayNumbers);
+
+        boolean existe = arrayList.contains("João");
+        if (existe){
+            System.out.println("Nome contém no array");
+        } else {
+            System.out.println("Nome nao contém no array");
+        }
+
+        boolean existe2 = arrayNumbers.contains(5);
+        if (existe2){
+            System.out.println("Numero contém no array");
+        } else {
+            System.out.println("Numero não contém no array  ");
+        }
+    }
+}
---
+++ src/com/tryCatch/tryCatch.java
@@ -0,0 +1,21 @@
+package com.tryCatch;
+
+public class tryCatch {
+    public static void main(String[] args){
+
+        try{
+            int[] array = new int[4];
+
+            System.out.println("== ANTES DA EXCEPTION ==");
+
+            array[5] = 1;
+        }
+        catch(ArrayIndexOutOfBoundsException exception){
+            System.out.println("=== !EXCEÇÃO! ===");
+//            System.exit(0);
+        }
+        finally {
+            System.out.println("== APÓS EXCEPTION ==");
+        }
+    }
+}
